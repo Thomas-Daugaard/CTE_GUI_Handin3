67771355444d3cda85ef53781df5a9bd87184e30
Egarder <61559853+Egarder@users.noreply.github.com>
2021-05-10 11:14:06 +0200

    dd

--- AgencyManagement/src/components/CreateManager.vue
+++ AgencyManagement/src/components/CreateManager.vue
@@ -1,32 +1,48 @@
 <template>
-    <div id="app">
-        <h3>Create Manager </h3> 
-        <input type="text" id="FirstName" name="FirstName">{{FirstName}}</input>
-        <input type="text" id="LastName" name="LastName">{{LastName}} </input>
-        <input type="text" id="Email" name="Email">{{Email}} </input>
+    <h3>Create Manager </h3>
+    <form>
+        <div id="app">
+            <input type="text" placeholder="Enter firstname" id="FirstName" name="FirstName" v-model="newmanager.FirstName"></input>
+            <input type="text" placeholder="Enter lastname" id="LastName" name="LastName" v-model="newmanager.LastName"> </input>
+            <input type="text" placeholder="Enter email" id="Email" name="Email" v-model="newmanager.Email"></input>
 
-        <input type="button" value="Create Manager" id="createbtn" name="createbtn" v-on:click="PostManager"/>
-    </div>
+            <input type="button" value="Create Manager" id="createbtn" name="createbtn" v-on:click="PostManager" />
+        </div>
+    </form>
 </template>
 
 <script>
     new Vue({
-        el: '#app'
+        el: '#app',
         data: {
-            FirstName: 'Enter FirstName',
-            LastName: 'Enter LastName',
-            Email: 'Enter Email'
-        }
+            newmanager{
+                FirstName: 'Enter FirstName',
+                LastName: 'Enter LastName',
+                Email: 'Enter Email'
+            },
+          
+        },
         methods:
         {
             PostManager: function(){
                 let url = "https://localhost:44368/api/Managers";
 
+                let temp = new EfManager()
+                {
+                        Email = Email,
+                        FirstName = FirstName,
+                        LastName = LastName
+                });
+
                 try{
+
+                    let response = await fetch(url, {});
+                    console.log(JSON.stringify(response));
 
                 }
 
             }
+  
         });
    
 </script>
