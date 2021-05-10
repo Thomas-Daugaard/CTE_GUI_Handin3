296b0b77e781a6ad19932fb5b62174d9d40bdde6
Egarder <61559853+Egarder@users.noreply.github.com>
2021-05-10 10:36:32 +0200

    aa

--- AgencyManagement/src/components/CreateManager.vue
+++ AgencyManagement/src/components/CreateManager.vue
@@ -4,6 +4,8 @@
         <input type="text" id="FirstName" name="FirstName">{{FirstName}}</input>
         <input type="text" id="LastName" name="LastName">{{LastName}} </input>
         <input type="text" id="Email" name="Email">{{Email}} </input>
+
+        <input type="button" value="Create Manager" id="createbtn" name="createbtn" v-on:click="PostManager"/>
     </div>
 </template>
 
@@ -15,7 +17,17 @@
             LastName: 'LastName',
             Email: 'Email'
         }
-    });
+        methods:
+        {
+            PostManager: function(){
+                let url = "https://localhost:44368/api/Managers";
+
+                try{
+
+                }
+
+            }
+        });
    
 </script>
 
