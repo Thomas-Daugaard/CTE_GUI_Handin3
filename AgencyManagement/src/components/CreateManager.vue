4feca25937d2b6c22c561bc8417d78b155f09619
Egarder <61559853+Egarder@users.noreply.github.com>
2021-05-10 10:27:32 +0200

    dd

--- AgencyManagement/src/components/CreateManager.vue
+++ AgencyManagement/src/components/CreateManager.vue
@@ -1,13 +1,22 @@
 <template>
     <div id="app">
         <h3>Create Manager </h3> 
-        <input type="text" id="FirstName" name="FirstName"> 
-        </input><input type="text" id="LastName" name="LastName"> </input>
-        <input type="text" id="Email" name="Email"> </input>
+        <input type="text" id="FirstName" name="FirstName">{{FirstName}}</input>
+        <input type="text" id="LastName" name="LastName">{{LastName}} </input>
+        <input type="text" id="Email" name="Email">{{Email}} </input>
     </div>
 </template>
 
 <script>
+    new Vue({
+        el: '#app'
+        data: {
+            FirstName: 'FirstName',
+            LastName: 'LastName',
+            Email: 'Email'
+        }
+    });
+   
 </script>
 
 <style scoped>
