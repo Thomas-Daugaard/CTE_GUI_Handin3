ac055d190cf88bb85c795b58bb75132763b63afe
Camilla <camilla.holmstoel@hotmail.com>
2021-05-10 10:10:38 +0200

    modsel add

--- AgencyManagement/src/components/NewModel.vue
+++ AgencyManagement/src/components/NewModel.vue
@@ -1,4 +1,55 @@
 <template>
+    <h1>Add model</h1>
+    <label>First Name: </label>
+    <input v-model="firstName" />
+
+    <label>Last Name: </label>
+    <input v-model="lastName" />
+
+    <label>Email</label>
+    <input v-model="email" />
+
+    <label>Phone Number</label>
+    <input v-model="phoneNo" />
+
+    <label>AddresLine1</label>
+    <input v-model="addresLine1" />
+
+    <label>AddresLine2</label>
+    <input v-model="addresLine2" />
+
+    <label>Zip-code</label>
+    <input v-model="zip" />
+
+    <label>City</label>
+    <input v-model="city" />
+
+    <label>Country</label>
+    <input v-model="country" />
+
+    <label>BirthDate</label>
+    <input v-model="birthDate" />
+
+    <label>Nationality</label>
+    <input v-model="nationality" />
+
+    <label>Height</label>
+    <input v-model="height" />
+
+    <label>ShoeSize</label>
+    <input v-model="shoeSize" />
+
+    <label>HairColor</label>
+    <input v-model="hairColor" />
+
+    <label>EyeColor</label>
+    <input v-model="eyeColor" />
+
+    <label>Comments</label>
+    <input v-model="comments" />
+
+    <label>Password</label>
+    <input v-model="password" />
 </template>
 
 <script>
