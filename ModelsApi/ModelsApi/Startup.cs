79bb108a447b28f79be26e722daa9f5878d9560f
Camilla <camilla.holmstoel@hotmail.com>
2021-05-11 13:29:30 +0200

    new model added to router

--- ModelsApi/ModelsApi/Startup.cs
+++ ModelsApi/ModelsApi/Startup.cs
@@ -32,7 +32,7 @@ namespace ModelsApi
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddCors();
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("ThomasConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("CamillaConnection")));
             services.AddControllers();
 
             // configure strongly typed settings objects
