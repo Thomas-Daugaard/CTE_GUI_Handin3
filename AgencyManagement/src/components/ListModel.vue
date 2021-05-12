a19c9e26f4d5502e7535e7ec443dfe9029f907a6
Camilla <camilla.holmstoel@hotmail.com>
2021-05-12 13:38:15 +0200

    ufærdigt

---
+++ AgencyManagement/src/components/ListModel.vue
@@ -0,0 +1,39 @@
+<template>
+    <form class="form">
+        <ul>
+            <li v-for="model in models" v-bind_key="model.id" v-bind:firstName="model.firstName" v-bind:lastName="model.lastName">
+                {{model.firstName}} {{model.lastName}}
+            </li>
+        </ul>
+    </form>
+</template>
+
+<script>
+    const vm = new Vue({
+        name: 'list-model',
+        data() {
+            models: []
+        },
+        methods: {
+            getModels: function () {
+                let url = "https://localhost:44368/api/ListModels";
+                fetch(url, {
+                    method: 'GET',
+                    credentials: 'include',
+                    headers: {
+                        'Authorization': 'Bearer ' + localStorage.getItem("token"),
+                        'Content-Type': 'application/json'
+                    }
+                }).then(responseJson => {
+                    this.response = responseJson);
+            })
+        .catch(error => alert('Something bad happened: ' + error));
+            }
+        }
+    }
+    )
+    
+</script>
+
+<style scoped>
+</style>
