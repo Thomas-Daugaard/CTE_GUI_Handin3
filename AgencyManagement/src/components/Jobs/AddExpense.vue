4ee42c025aae735e08f90bdf2354f6357a02899b
Camilla <camilla.holmstoel@hotmail.com>
2021-05-15 23:15:41 +0200

    addExpense

--- AgencyManagement/src/components/Jobs/AddExpense.vue
+++ AgencyManagement/src/components/Jobs/AddExpense.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-        <form method="post" class="form">
-            <h1>Add Expense</h1>
-            <input v-model="newExpense.date" class="name formEntry" placeholder="Enter Date" />
-            <input v-model="newExpense.text" class="name formEntry" placeholder="Enter Text" />
-            <input v-model="newExpense.amount" class="name formEntry" placeholder="Enter Amount" />
-            <input type="button" class="submit formEntry" value="Add Expense" v-on:click="postExpense" />
-            <router-link :to="'/jobs/index'"><input type="button" class="submit formEntry" value="Back to jobs" /></router-link>
+        <form class="form">
+            <div>
+                <h1>Add Expense</h1>
+                <input v-model="newExpense.date" class="name formEntry" type="datetime" placeholder="Enter Date" />
+                <input v-model="newExpense.text" class="name formEntry" type="text" placeholder="Enter Text" />
+                <input v-model="newExpense.amount" class="name formEntry" type="text" placeholder="Enter Amount" />
+                <input type="button" class="submit formEntry" value="Add Expense" v-on:click="postExpense" />
+                <router-link :to="'/jobs/index'"><input type="button" class="submit formEntry" value="Back to jobs" /></router-link>
+            </div>
         </form>
     </div>
 </template>
@@ -14,13 +16,19 @@
 <script>
     export default {
         name: 'add-expense',
+        props: ['jobid', 'modelid'],
         data() {
             return {
+                model: null,
+                job: null,
                 newExpense: {
                     date: '',
                     text: '',
-                    amount: ''
+                    amount: '',
+                    modelid: this.modelid,
+                    jobid: this.jobid
                 }
+                
             }
         },
         methods: {
@@ -33,7 +41,7 @@
                         'Authorization': 'Bearer' + " " + localStorage.getItem("token"),
                         'Content-Type': 'application/json'
                     }),
-                    body: JSON.stringify(this.form.newModel)
+                    body: JSON.stringify(this.newExpense)
                 }).then(res => res.json()).catch(error => alert("Error" + error));
             }
         }
